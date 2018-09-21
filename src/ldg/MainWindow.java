7d34d78d98f4fe371925e94d30857f4c7cb5f120
Erikas <erikmnkl@gmail.com>
2018-09-21 05:16:40 +0300

    rm unused import

--- src/ldg/MainWindow.java
+++ src/ldg/MainWindow.java
@@ -3,7 +3,6 @@ package ldg;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
